59b3e73e71be86aed3c7d79113760f5409b9e5d4
lau <1091238205@qq.com>
2021-09-04 22:37:19 +0800

    test java,scala,maven,log4j,git,github...

--- src/main/java/testjava.java
+++ src/main/java/testjava.java
@@ -12,5 +12,19 @@ public class testjava {
         logger.info("This is info message.");
         // 记录error级别的信息
         logger.error("This is error message.");
+        System.out.println(add(1,6));
     }
+    
+    /**
+     * @Author: liuminghui
+     * @Description: //TODO
+     * @Date: 22:34 2021/9/4
+     * @Param [a, b]
+     * @Return int
+     **/
+    public static int add(int a,int b){
+        return a+b;
+    }
+
 }
+
--- src/main/scala/testScala.scala
+++ src/main/scala/testScala.scala
@@ -1,4 +1,5 @@
 import org.apache.log4j.Logger;
+
 object testScala {
 
   private val logger = Logger.getLogger(testScala.getClass);
@@ -11,5 +12,18 @@ object testScala {
     logger.info("This is info message.")
     // 记录error级别的信息
     logger.error("This is error message.")
+    println(add(1,6))
   }
+
+  /**
+   * @Author: liuminghui
+   * @Description: //TODO
+   * @Date: 22:33 2021/9/4
+   * @Param [a, b]
+   * @Return int
+   **/
+  def add(a:Int,b:Int):Int={
+    return a+b;
+  }
+
 }
